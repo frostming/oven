d0300fe10adc68b30ccbdfa7779f1fb7833f8303
tssujt <github@tssujt.xyz>
2024-04-26 13:38:57 +0800

    Fix typo in title

--- app/root.tsx
+++ app/root.tsx
@@ -23,7 +23,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Oven - Explor Python packages</title>
+        <title>Oven - Explore Python packages</title>
         <Meta />
         <Links />
       </head>
