3199dd703b5b6f5cc6de5450ce2b25d1a011c71d
Frost Ming <me@frostming.com>
2024-04-29 17:42:58 +0800

    fix: Adjust the default styles for img element in Markdown
    Fixes #5
    
    Signed-off-by: Frost Ming <me@frostming.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -71,6 +71,17 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            img: {
+              marginTop: '0',
+              marginBottom: '0',
+              display: 'inline-block',
+            },
+          },
+        },
+      },
     },
   },
   plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
