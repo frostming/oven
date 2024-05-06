7198e49279b1f5d7f83d43279d928d95b12a43cd
Frost Ming <me@frostming.com>
2024-05-06 12:42:14 +0800

    fix: render GFM
    Signed-off-by: Frost Ming <me@frostming.com>

--- app/components/Markdown.tsx
+++ app/components/Markdown.tsx
@@ -1,9 +1,10 @@
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
+import remarkGfm from 'remark-gfm'
 
 export default function Markdown({ children }: { children: string }) {
   return (
-    <ReactMarkdown rehypePlugins={[rehypeRaw]}>
+    <ReactMarkdown rehypePlugins={[rehypeRaw, remarkGfm]}>
       {children}
     </ReactMarkdown>
   )
